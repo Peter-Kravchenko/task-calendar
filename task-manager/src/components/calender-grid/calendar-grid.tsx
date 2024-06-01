084c30623f43bf58d70e374f4d4a2037098654e8
Петр Кравченко <103353719+Peter-Kravchenko@users.noreply.github.com>
2024-06-01 19:42:07 +0300

    feat: Подключит moment.js, добавитотображение актуального календаря

--- task-manager/src/components/calender-grid/calendar-grid.tsx
+++ task-manager/src/components/calender-grid/calendar-grid.tsx
@@ -1,16 +1,27 @@
 import './calendar-grid.css';
 
-function CalendarGrid() {
-  const totalDays = 42;
-  const daysArray = [...Array(totalDays).keys()];
+function CalendarGrid({ today, startDate, totalDays }) {
+  const day = startDate.clone();
+  const daysMap = [...Array(totalDays)].map(() => day.add(1, 'day').clone());
 
   return (
     <div className="grid-wrapper">
-      {daysArray.map((day) => (
-        <div className="cell-wrapper weekday" key={day}>
-          {day}
-        </div>
-      ))}
+      {daysMap.map((day, index) => {
+        const date = day.date();
+        const isToday = today.isSame(day, 'day');
+        const isWeekend = day.day() === 0 || day.day() === 6;
+
+        return (
+          <div
+            key={index}
+            className={`cell-wrapper ${isToday ? 'today' : ''} ${
+              isWeekend ? 'weekend' : 'weekday'
+            }`}
+          >
+            <div className="day-wrapper">{date}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- task-manager/src/pages/calendar-page/calendar-page.tsx
+++ task-manager/src/pages/calendar-page/calendar-page.tsx
@@ -1,13 +1,18 @@
+import moment from 'moment';
 import CalendarGrid from '../../components/calender-grid/calendar-grid';
 import Display from '../../components/display/display';
 import Header from '../../components/header/header';
 
 function CalendarPage() {
+  const today = moment();
+  const startDate = today.clone().startOf('month').startOf('week');
+  const totalDays = 42;
+
   return (
     <div>
       <Header />
       <Display />
-      <CalendarGrid />
+      <CalendarGrid today={today} startDate={startDate} totalDays={totalDays} />
     </div>
   );
 }
