19872a505f5d303161eb610781d14943d5940ff3
Петр Кравченко <103353719+Peter-Kravchenko@users.noreply.github.com>
2024-06-01 15:40:43 +0300

    Init

---
+++ task-manager/src/components/app/app.tsx
@@ -0,0 +1,12 @@
+import CalendarPage from '../../pages/calendar-page/calendar-page';
+import './app.css';
+
+function App() {
+  return (
+    <>
+      <CalendarPage />
+    </>
+  );
+}
+
+export default App;
---
+++ task-manager/src/pages/calendar-page/calendar-page.tsx
@@ -0,0 +1,5 @@
+function CalendarPage() {
+  return <div>test</div>;
+}
+
+export default CalendarPage;
